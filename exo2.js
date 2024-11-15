7262881783d731d7924661287be24e2cf81cdbc2
Dangreville-R <rdangreville@gmail.com>
2024-11-15 10:28:00 +0100

    exo2

---
+++ exo2.js
@@ -0,0 +1,10 @@
+//chaine de caractère
+var maVariableString = "toto";
+//nombre
+var maVariableNumerique = 100;
+//des tableaux
+var MaVariableTableau = ['Apple', 'Banane'];
+//des objets (ici la fenetre du navigateur)
+var MaVariableObjet = window
+
+alert(maVariableString + maVariableNumerique +  MaVariableTableau[0]+ MaVariableObjet.location.pathname);
