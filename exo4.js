65106ba5948d59c4affb7e2512c27a8b57e74a81
Dangreville-R <rdangreville@gmail.com>
2024-11-15 10:50:09 +0100

    exo4finit

---
+++ exo4.js
@@ -0,0 +1,11 @@
+//chaine de caractere
+var maVariableString = "toto";
+//nombre
+var maVariableNumerique = prompt("entrer un nombre",100);
+
+if (maVariableString == "toto" && maVariableNumerique !=200 || maVariableNumerique <=100) {
+    alert("je suis le cas 1");
+} else {
+    alert("je suis le cas 2");
+    // il faut tapper la valeur 200 pour obtenir le cas 2. 
+}
