ff9767ff0f0138988790141b5ac05b14ebcc27c2
Dangreville-R <rdangreville@gmail.com>
2024-11-20 11:56:59 +0100

    exo5 finis

--- exo5.js
+++ exo5.js
@@ -1,3 +1,10 @@
-if (screen.width < 960) {
-    alert("attention screen trop petit");
+// Fonction qui vérifie la largeur de la fenêtre
+function checkWindowSize() {
+    if (window.innerWidth < 600) {
+        alert('La largeur de la fenêtre est inférieure à 300px !');
+    }
 }
+// Ajout d'un écouteur d'événement pour détecter les redimensionnements
+window.addEventListener('resize', checkWindowSize);
+// Vérifie immédiatement si la fenêtre est déjà inférieure à 300px
+checkWindowSize();
